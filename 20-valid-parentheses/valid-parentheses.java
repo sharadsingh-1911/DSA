0ff37f5378a240bde50da443a3af6265e6117b0f
sharadsingh-1911 <sharadkrsingh1911@gmail.com>
2026-06-09 07:54:37 +0530

    Time: 158 ms (5.76%) | Memory: 46.9 MB (5.77%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isValid(String s) {
+        while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
+        }
+        if(s.length()==0){
+            return true;
+        }
+        return false;
+    }
+}
